976bb959ba3c245ca32b194023c367e6c9c264f8
Mujayt Abdu <Mujiayitiabdu@gmail.com>
2022-11-14 14:52:00 -0500

    two leetcode challange is done

---
+++ src/main/java/leetCode/RunningSumOf1dArray_Day1.java
@@ -0,0 +1,31 @@
+package leetCode;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class RunningSumOf1dArray_Day1 {
+
+	public static void main(String[] args) {
+
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		System.out.println(Arrays.toString(twoSum(nums, target)));
+
+	}
+
+	static int[] twoSum(int[] nums, int target) {
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+
+					return new int[] { i, j };
+				}
+			}
+		}
+
+		return null;
+
+	}
+
+}
